3699f3645d32d632de03b0c412f8b2f5d301125f
M. Kram <mark@mastop.net>
2025-09-08 00:03:06 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Server, Smartphone, FileText, Settings } from 'lucide-react-native';
+import { Chrome as Home, Server, Smartphone, FileText, Settings } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
